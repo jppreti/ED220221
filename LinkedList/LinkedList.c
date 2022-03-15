6327263fbd7e65d72f34b5136f2e044a6cc92d1e
TmprS-VitorC <eng.vitorcl@gmail.com>
2022-03-15 08:46:52 -0400

    Implementação LinkedList ; Trabalhando na implementação DoublyLinkedList;

--- LinkedList/LinkedList.c
+++ LinkedList/LinkedList.c
@@ -1,6 +1,40 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "LinkedList.h"
+
+bool compara(void*, void*);
+
+int main() {
+    LinkedList list;
+    init(&list);
+    
+    int *aux = (int *)malloc(sizeof(int));
+    *aux=1;
+    enqueue(&list, aux);
+    aux = (int *)malloc(sizeof(int));
+    *aux=2;
+    enqueue(&list, aux);
+    aux = (int *)malloc(sizeof(int));
+    *aux=3;
+    enqueue(&list, aux);
+    
+    printf("%d\n",*((int*)first(&list)));
+    printf("%d\n",indexOf(&list,aux,compara));
+    printf("%d\n",*((int*)getPos(&list,2)));
+    printf("%d\n",*((int*)dequeue(&list)));
+    printf("%d\n",*((int*)dequeue(&list)));
+    printf("%d\n",*((int*)dequeue(&list)));
+    //printf("%d\n",*((int*)dequeue(&stack)));
+    
+    return EXIT_SUCCESS;
+}
+
+bool compara(void *data1, void *data2) {
+    int *d1 = (int*)data1;
+    int *d2 = (int*)data2;
+    
+    return (*d1==*d2)?true:false;
+}
 
 void init(LinkedList *list) {
     list->first=NULL;
@@ -201,3 +235,4 @@ bool removeData(LinkedList *list, void *data, compare equal) {
         }
     }
 }
+
