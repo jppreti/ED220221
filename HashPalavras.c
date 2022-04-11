addfffd4014ddd8de8e718d33420e1f03e5a7eb0
Eriseu <eriseu@gmail.com>
2022-04-11 14:35:30 +0000

    Ajustes

--- HashPalavras.c
+++ HashPalavras.c
@@ -3,8 +3,10 @@
 #include <string.h>
 #include "Hash.h"
 #include "log.h"
-#define LINHA 4
-#define COLUNA 3
+#define TAMANHO_IMG 320
+#define TAMANHO_PX 10
+#define LINHA 1024
+#define COLUNA TAMANHO_PX
 
 typedef struct Palavras {
     char palavra[100];
@@ -17,11 +19,13 @@ bool comparaChaves(void *key, void *data) {
     return (strcmp (chave, c->palavra) == 0)?true:false;
 }
 
-void matriz_ponteiro(int **mtr, size_t linhas, size_t colunas) {
+int matriz_ponteiro(int **mtr, size_t linhas, size_t colunas) {
      for (int lin = 0; lin < linhas; lin++) {
-        for (int col = 0; col < colunas; col++) printf("\t%d", mtr[lin][col]);
-  
-        printf("\n");
+        for (int col = 0; col < colunas; col++) {
+			printf("%d", mtr[lin][col]);
+			//return mtr[lin][col];
+		}
+        //printf("\n");
     }
 }
 
@@ -33,8 +37,9 @@ void main()
 //  printf("tam: %d",hashes.size);
   FILE *arq;
   char Linha[100];
-  char *result;    
-  int divisor, i, j, k; 
+  char *result; 
+  char texto[20];
+  int divisor, i, j, k, z; 
 	
   //clrscr();
   // Abre um arquivo TEXTO para LEITURA
@@ -58,36 +63,62 @@ void main()
       i++;
   }
 
-	//printf("\n tam: %d", hashes.size);
+	printf("\n tam: %d\n", hashes.size);
 
   	fclose(arq);
 	
 	int maior = 0;
-  	for(i=0; i<1024; i++) {
-  		//printf("Qtde de elem do hash %d: %d\n",i,hashes.hashes[i].size);
+  	for(i=0; i<LINHA; i++) {
+  		printf("Qtde de elem do hash %d: %d\n",i,hashes.hashes[i].size);
+			
   		if (hashes.hashes[i].size > maior) {
 			maior = hashes.hashes[i].size;
 		}	
-		
 	}
 	//printf("maior = %d\n", maior);
 	divisor = 255/maior;
 	//printf("divisao do maior por 255 %d\n", 255/maior);
 	
 	arq = fopen("espalhamento.ppm", "w");
-	fputs("P3\n320 320\n255\n", arq);
 	
-	for(int i=0; i<1024; i++){
-		for(int k=0; k<10; k++){
-			for(int j=0; j<10; j++){
-				char buffer[50];
-
-				sprintf(buffer, "%d %d %d\n", hashes.hashes[i].size*divisor, 0, 0);
+	sprintf(texto, "P3\n%d %d\n255\n", TAMANHO_IMG, TAMANHO_IMG); 
+	fputs(texto, arq);
+	
+	char buffer[50];
+	
+	int **mtr = malloc(LINHA * sizeof(*mtr));
+	
+	for (int lin = 0, cont = 0; lin < LINHA; lin++) {
+		mtr[lin] = malloc(COLUNA * sizeof(*mtr[lin]));
+		for (int i = 0; i < hashes.size/(TAMANHO_IMG*TAMANHO_PX); i++) {		
+			for (int col = 0; col < COLUNA; col++) {
+				mtr[lin][col] = hashes.hashes[lin].size*divisor;
+				sprintf(buffer, "%d %d %d\n", mtr[lin][col], 0, 0);
 				fputs(buffer, arq);
-				printf("\n%d\t%d", j, hashes.hashes[i].size*divisor);
 			}
+			fputs("0 0 0\n", arq);
 		}
-		printf(" - %d", i);
+		//fputs("0 0 0\n", arq);
 	}
+	for (int i = 0; i < 10; i++) {
+		matriz_ponteiro(mtr, LINHA, COLUNA);
+	}
+	printf("\n%d",hashes.size/(TAMANHO_IMG*TAMANHO_PX));
+		//for(int i=0; i<LINHA; i++){
+			//for(int k=0; k<10; k++){
+				//for(int z=0; z<32; z++){
+					//for(int j=0; j<10; j++){
+						//char buffer[50];
+						//sprintf(buffer, "%d %d %d\n", hashes.hashes[i].size*divisor, 0, 0);
+						//sprintf(buffer, "%d %d %d\n", matriz_ponteiro(mtr, LINHA, COLUNA), 0, 0);
+						//fputs(buffer, arq);
+						//printf("\n%d\t%d", j, hashes.hashes[i].size*divisor);
+					//}
+					//printf(" - %d", i);
+				//}	
+			//}	
+		//}
+	
 	fclose(arq);
+
 }
